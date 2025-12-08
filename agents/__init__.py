c7b7ba46acad26997e98ac66a22584ad5a8a4d3f
ojayWillow <og.vitols@gmail.com>
2025-12-08 15:51:21 +0200

    Init: Create agents module

---
+++ agents/__init__.py
@@ -0,0 +1,8 @@
+"""Agents module for GOAT Data Analyst.
+
+Contains specialized agents for data analysis, visualization, and insights.
+"""
+
+from .orchestrator import Orchestrator
+
+__all__ = ['Orchestrator']
