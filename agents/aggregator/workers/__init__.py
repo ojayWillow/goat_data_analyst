f29eff63d41fdc9d170eb6a56d98412348b0251c
ojayWillow <og.vitols@gmail.com>
2025-12-10 13:58:40 +0200

    fix: Correct CrosstabWorker case to CrossTabWorker

--- agents/aggregator/workers/__init__.py
+++ agents/aggregator/workers/__init__.py
@@ -1,4 +1,4 @@
-﻿from .base_worker import BaseWorker, WorkerResult, ErrorType
+from .base_worker import BaseWorker, WorkerResult, ErrorType
 from .groupby import GroupByWorker
 from .pivot import PivotWorker
 from .crosstab import CrossTabWorker
