ce3690c04b2e93c46affc688f9b130b3d8164900
ojayWillow <og.vitols@gmail.com>
2025-12-10 17:22:10 +0200

    feat: Update Anomaly Detector workers __init__ with 4 new workers

--- agents/anomaly_detector/workers/__init__.py
+++ agents/anomaly_detector/workers/__init__.py
@@ -1,15 +1,24 @@
-"""Anomaly Detector Workers - Specialized workers for anomaly detection."""
+"""Anomaly Detector Workers - Specialized workers for anomaly detection.
+
+Week 1 Day 3 Workers (4):
+- LOFAnomalyDetector: Local Outlier Factor algorithm
+- OneClassSVMAnomalyDetector: One-Class SVM algorithm
+- IsolationForestAnomalyDetector: Isolation Forest algorithm
+- EnsembleAnomalyDetector: Ensemble voting method
+"""
 
 from .base_worker import BaseWorker, WorkerResult, ErrorType
-from .statistical import StatisticalWorker
-from .isolation_forest import IsolationForestWorker
-from .multivariate import MultivariateWorker
+from .lof_anomaly_detector import LOFAnomalyDetector
+from .ocsvm_anomaly_detector import OneClassSVMAnomalyDetector
+from .isolation_forest_anomaly_detector import IsolationForestAnomalyDetector
+from .ensemble_anomaly_detector import EnsembleAnomalyDetector
 
 __all__ = [
     "BaseWorker",
     "WorkerResult",
     "ErrorType",
-    "StatisticalWorker",
-    "IsolationForestWorker",
-    "MultivariateWorker",
+    "LOFAnomalyDetector",
+    "OneClassSVMAnomalyDetector",
+    "IsolationForestAnomalyDetector",
+    "EnsembleAnomalyDetector",
 ]
